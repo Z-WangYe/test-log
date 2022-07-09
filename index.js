a602dc48118fe6609b93f5228642e349c87520ec
wangye <wangye@agree.com.cn>
2022-07-09 13:03:19 +0800

    修改代码

--- index.js
+++ index.js
@@ -2,16 +2,9 @@ const { configure, getLogger, addLayout, shutdown } = require('log4js');
 const path = require('path');
 const logSettingFilePath = path.join(__dirname, 'log4js.config.json');
 configure(logSettingFilePath);
-var logger = getLogger('App');
+let logger = getLogger('App');
 logger.info('[initLogger]logSettingFilePath:' + logSettingFilePath);
 
-let count = 0
-const interval = setInterval(() => {
-    if(count< 10) {
-        logger.info("11111111")
-        count++
-    } else {
-        shutdown()
-        clearInterval(interval)
-    }
-}, 1000)
+setInterval(()=>{
+    logger.info("111111")
+},1000)
